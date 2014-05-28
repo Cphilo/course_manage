46417f1d191532724b18bfa06e0840b621cb53cf
cphilo <cphilo@foxmail.com>
2014-05-28 17:00:51 +0800

    basic v

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,15 @@ var render_stu = function(username, res) {
         });
     });
 };
+var render_ta = function(username, res) { 
+    var sql = util.format("select * from class, course where tname='%s' and class.cno=course.cno", username);
+    conn.query(sql, function(err, rows) {
+        if(err)throw err;
+        else {        
+            res.render("teacher", { username: username, classes: rows });
+        }
+    });
+};
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -38,13 +47,7 @@ router.post('/login', function(req, res) {
             if(rows[0].role == "s") {
                 render_stu(username, res);
             } else {
-                var sql = util.format("select * from class where tname='%s' ", username);
-                conn.query(sql, function(err, rows) {
-                    if(err)throw err;
-                    else {        
-                        res.render("teacher", { username: username, classes: rows });
-                    }
-                });
+                render_ta(username, res);
             }
         } else {
             res.render("index", {message: "*请重新输入用户名和密码", title: '课程管理系统'});  
@@ -53,7 +56,7 @@ router.post('/login', function(req, res) {
 });
 
 //Here need set session when user login.
-//Or the simulated add course post request can attack this website.
+//Or the simulated add record post request can attack this website.
 //Now I just try to make things simple.
 router.post("/:user/addRecord", function(req, res) {
     var user = req.param("user");
@@ -64,6 +67,52 @@ router.post("/:user/addRecord", function(req, res) {
         if(err)throw err;
         render_stu(user, res);
     });
+});
+
+//Same attack like the above add record request.
+//Need set user login session.
+router.get("/:clsno/addGrade", function(req, res) {
+    var clsno = parseInt(req.param("clsno"));
+    var sql = util.format("select * from record where clsno=%d", clsno);
+    var cls_sql = util.format("select * from class, course where clsno=%d and class.cno=course.cno", clsno);
+    conn.query(sql, function(err, records) {
+        if(err) throw err;
+        conn.query(cls_sql, function(err, clss) {
+            if(err) throw err;
+            res.render("addGrade", {
+                records: records,
+                cls: clss[0]
+            });
+        });         
+    });
+});
+
+var handle_mul_update = function(cnt, l, res, clsno) {
+    if(cnt == l) {
+        var sql = util.format("select * from class where clsno=%d", clsno);
+        conn.query(sql, function(err, rows) {
+            if(err) throw err;
+            render_ta(rows[0].tname, res);
+        });     
+    }
+    
+};
+
+router.post("/:clsno/commitGrade", function(req, res) {
+    var clsno = parseInt(req.param("clsno"));
+    var snames = req.param("sname");
+    var grades = req.param("grade");
+    console.log(grades);
+    var usql = '';
+    var cnt = 0;
+    for(var i=0;i<snames.length;i++) {
+        usql = util.format("update record set grade=%d where clsno=%d and sname='%s'", parseFloat(grades[i]), clsno, snames[i]);
+        conn.query(usql, function(err, rows) {
+            if(err)throw err;
+            cnt += 1;
+            handle_mul_update(cnt, snames.length, res, clsno);
+        });
+    }
 });
 
 
